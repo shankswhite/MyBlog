2c89eb53f97291f727580338a2f836977d40e659
Levon <zhaoxiaofengpy@gmail.com>
2024-07-01 04:53:03 -0700

    Header v1 Hero v1

--- myblog/src/components/Hero/HeroComponent.tsx
+++ myblog/src/components/Hero/HeroComponent.tsx
@@ -3,32 +3,76 @@ import React from 'react'
 import css from './HeroComponent.module.scss'
 import '../../styles/global.scss'
 
+import { motion } from 'framer-motion'
+// @ts-ignore
+import { staggerContainer, fadeIn, slideIn } from '../../utils/motion'
+
+
+
 function HeroComponent() {
   return (
     <section className={`paddings ${css.wrapper}`}>
-        <div className={`innerWidth ${css.container}`}>
+        <motion.div 
+        variants={staggerContainer}
+        initial="hidden"
+        whileInView="show"
+        viewport={{once: false, amount: 0.25}}
+
+        className={`innerWidth ${css.container}`}>
 
             <div className={css.upperElements}>
-                <span>
+
+                <motion.span 
+                variants={fadeIn("right", "tween", 0.2, 1)}
+                className='primaryText'>
+
                     Hey There, <br />I'm Levon.
-                </span>
-                <span>
+                </motion.span>
+                <motion.span 
+                variants={fadeIn("left", "tween", 0.2, 1)}
+                className='secondaryText'>
                     I focus on <br />AI and Game Devlopment.
-                </span>
+                </motion.span>
             </div>
 
+            {/* Email */}
+            <motion.a 
+            variants={fadeIn("right", "tween", 0.2, 1)}
+            className={css.email} href="mailto:zhao.levon@gmail.com">
+                zhao.levon@gmail.com
+            </motion.a>
+
+            {/* Photo */}
+            <motion.div 
+            variants={fadeIn("up", "tween", 0.3, 1)}
+            className={css.person}>
+                <motion.img 
+                variants={slideIn("up", "tween", 0.4, 1.2)}
+                src="./person.png" alt="" />
+            </motion.div>
+
+            {/* Welcome Text */}
             <div className={css.lowerElements}>
-                <div className={`css.experience`}>
-                    <div className="primaryText">3 Years Game Designer</div>
-                    <div className="primaryText">1 Year Data Analyst</div>
-                </div>
-                <div className={css.certificate}>
+                <motion.div 
+                variants={fadeIn("right", "tween", 0.2, 1)}
+                className={`css.experience`}>
+                    {/* <div className="primaryTextUnbold"><b>3</b> Years &nbsp;Game Designer</div> */}
+                    <span className='primaryText'>3 Years</span>
+                    <span className='primaryTextUnbold'>&nbsp;Game Designer</span>
+                    <br />
+                    <span className='primaryText'>1 Year</span>
+                    <span className='primaryTextUnbold'>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Data&nbsp;&nbsp; Anaylst&nbsp;&nbsp;&nbsp;</span>
+                    {/* <div className="primaryTextUnbold"><b>1</b> Year&nbsp;&nbsp;&nbsp;&nbsp;Data &nbsp;&nbsp;Analyst</div> */}
+                </motion.div>
+                <motion.div 
+                variants={fadeIn("left", "tween", 0.2, 1)}
+                className={css.certificate}>
                     <img src="./certificate.png" alt="" />
                     <span>Certified AWS Cloud Practitioner</span>
                     <span>Passed CFA Level 1 Test</span>
-                </div>
+                </motion.div>
             </div>
-        </div>
+        </motion.div>
     </section>
   )
 }
