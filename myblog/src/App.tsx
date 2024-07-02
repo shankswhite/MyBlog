dcd4958f27e5616f591ea94f7a0355348d1b57df
Levon <zhaoxiaofengpy@gmail.com>
2024-07-02 05:22:59 -0700

    Added showcase part

--- myblog/src/App.tsx
+++ myblog/src/App.tsx
@@ -1,4 +1,5 @@
 import './App.css'
+import ExpertiesComponent from './components/Experties/ExpertiesComponent';
 import HeaderComponent from './components/Header/HeaderComponent';
 import HeroComponent from './components/Hero/HeroComponent';
 import WIPComponent from './components/WIP/WIPComponent';
@@ -11,6 +12,7 @@ function App() {
   return <div className={`bg-primary ${css.container}`}>
     <HeaderComponent />
     <HeroComponent />
+    <ExpertiesComponent />
     <WIPComponent />
   </div>
 }
